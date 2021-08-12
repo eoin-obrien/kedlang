3621ca1073c58780b1476c393959b120e0ac5cbe
Eoin O'Brien <eoin@tracworx.ai>
2021-08-12 01:33:29 +0100

    Add repr for ast nodes

--- src/kedlang/ast.py
+++ src/kedlang/ast.py
@@ -112,8 +112,7 @@ class Program(KedAST):
         self.statements = statements
 
     def __repr__(self) -> str:
-        statements = '\n'.join(self.statements)
-        return f"<{self.__class__.__name__} {statements}>"
+        return f"<{self.__class__.__name__} {self.statements}>"
 
 
 class Declare(Statement):
@@ -123,7 +122,7 @@ class Declare(Statement):
         self.variable, self.initializer = variable, initializer
 
     def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} {self.variable}={self.initializer}>"
+        return f"<{self.__class__.__name__} {self.variable} {self.initializer}>"
 
 
 class FunctionDef(Statement):
@@ -131,20 +130,32 @@ class FunctionDef(Statement):
     def __init__(self, name: Name, params: List[Variable], body: Statement) -> None:
         self.name, self.params, self.body = name, params, body
 
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.name} {self.params} {self.body}>"
+
 
 class Delete(Statement):
     def __init__(self, variable: Variable) -> None:
         self.variable = variable
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.variable}>"
 
 
 class Compound(Statement):
     def __init__(self, children: List[Statement]) -> None:
         self.children = children
 
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.children}>"
+
 
 class Expr(Statement):
     def __init__(self, value: Expression) -> None:
         self.value = value
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.value}>"
 
 
 class If(Statement):
@@ -158,11 +169,17 @@ class If(Statement):
         self.body = body
         self.orelse = orelse
 
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.test} {self.body} {self.orelse}>"
+
 
 class While(Statement):
     def __init__(self, test: Expression, body: List[Statement]) -> None:
         self.test = test
         self.body = body
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.test} {self.body}>"
 
 
 class Continue(Statement):
@@ -177,20 +194,32 @@ class Return(Statement):
     def __init__(self, value: Optional[Expression] = None) -> None:
         self.value = value
 
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.value}>"
+
 
 class Print(Statement):
     def __init__(self, value: Expression) -> None:
         self.value = value
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.value}>"
 
 
 class Import(Statement):
     def __init__(self, name: Expression, is_strict: bool = False) -> None:
         self.name, self.is_strict = name, is_strict
 
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.name} {self.is_strict}>"
+
 
 class Assign(Expression):
     def __init__(self, variable: Variable, expression: Expression) -> None:
         self.variable, self.expression = variable, expression
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.variable} {self.expression}>"
 
 
 class BinaryOp(Expression):
@@ -198,35 +227,56 @@ class BinaryOp(Expression):
         self.left, self.right = left, right
         self.token = self.op = op
 
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.left} {self.op} {self.right}>"
+
 
 class UnaryOp(Expression):
     def __init__(self, op: UnaryOperator, operand: Expression) -> None:
         self.op, self.operand = op, operand
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.op} {self.operand}>"
 
 
 class Attribute(Expression):
     def __init__(self, value: Expression, attr: str) -> None:
         self.value, self.attr = value, attr
 
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.value} {self.attr}>"
+
 
 class Subscript(Expression):
     def __init__(self, value: Expression, index: Expression) -> None:
         self.value, self.index = value, index
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.value} {self.index}>"
 
 
 class Call(Expression):
     def __init__(self, func: Expression, args: List[Expression]) -> None:
         self.func, self.args = func, args
 
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.func} {self.args}>"
+
 
 class IsDeclared(Expression):
     def __init__(self, variable: Variable) -> None:
         self.variable = variable
 
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.variable}>"
+
 
 class Constant(Expression):
     def __init__(self, token: Token) -> None:
         self.token = token
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.token.value}>"
 
 
 class NoOp(Expression):
@@ -236,6 +286,9 @@ class NoOp(Expression):
 class Sleep(Expression):
     def __init__(self, value: Expression) -> None:
         self.value = value
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.value}>"
 
 
 class Exit(Expression):
