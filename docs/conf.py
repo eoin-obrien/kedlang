d3bf8d9e0e5103f5a789fcb9678792f9f3534cd7
Eoin O'Brien <eoin@tracworx.ai>
2021-08-16 01:51:44 +0100

    Configure scm version without local scheme

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'kedlang'
-copyright = u'2021, Eoin O'Brien'
+copyright = u'2021, Eoin O\'Brien'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -249,7 +249,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'user_guide.tex', u'kedlang Documentation',
-   u'Eoin O'Brien', 'manual'),
+   u'Eoin O\'Brien', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
--- setup.py
+++ setup.py
@@ -13,11 +13,15 @@ from pkg_resources import VersionConflict, require
 from setuptools import setup
 
 try:
-    require('setuptools>=38.3')
+    require("setuptools>=38.3")
 except VersionConflict:
     print("Error: version of setuptools is too old (<38.3)!")
     sys.exit(1)
 
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    setup(
+        use_pyscaffold=True,
+        use_scm_version={"local_scheme": "no-local-version"},
+        setup_requires=["pyscaffold", "setuptools_scm"],
+    )
